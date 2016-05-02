8f461655d6c674befd9d8980f3a79b3db1086a8f
Leandro Scott <lsrzj@yahoo.com>
2016-05-02 19:21:02 -0300

    Utilizando o método put para update
    OBS: era para mandar no mesmo commit, apenas esqueci de incluir o
    arquivo

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,6 +21,6 @@ Route::post('client', 'ClientController@store');
 
 Route::get('client/{id}', 'ClientController@show');
 
-Route::post('client/{id}', 'ClientController@update');
+Route::put('client/{id}', 'ClientController@update');
 
 Route::delete('client/{id}', 'ClientController@destroy');
